eed2d0de4273ffcd3d0dd626addcc5c1fab60482
Abhishek <abhishekm.21.beis@acharya.ac.in>
2024-12-03 17:30:29 +0530

    updated

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,11 @@
-// import './App.css';
-
-// function App() {
-//   return (
-//     <>
-//     </>
-//   );
-// }
-
-// export default App;
-
-
-import './App.css';
-import { Routes, Route } from 'react-router-dom'; // Import Routes and Route
-// import OptionPage from './components/OptionPage'; // Import OptionPage
-import CreateExamPage from './components/CreateExamPage'; // Import the new page component
-import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import StudentForm from "./components/StudentForm";
 
 function App() {
   return (
-    <Routes> {/* Set up routing */}
-      {/* <Route path="/" element={<OptionPage />} /> */}
-      <Route path="/create-exam" element={<CreateExamPage />} /> Define the route for Create Exam
-    </Routes>
+    <div>
+      <StudentForm />
+    </div>
   );
 }
 
